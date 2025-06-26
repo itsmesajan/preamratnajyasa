18c4ddb104d687a8bb1b5c9a66f34e44c0899dc4
Sajan Maharjan <itsmesajanmaharjan@gmail.com>
2025-06-26 22:20:46 +0545

    [EDIT] DB arranged

--- includes/common.php
+++ includes/common.php
@@ -1,7 +1,11 @@
-<!--establish the connection to database, and start the session-->
 <?php
-$con = mysqli_connect("localhost", "root", "", "store") or die($mysqli_error($con));
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-?>
