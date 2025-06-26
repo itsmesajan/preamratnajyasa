2f032a3f79868317fbc2bab286517b9019ffd957
Sajan Maharjan <itsmesajanmaharjan@gmail.com>
2025-06-26 22:34:11 +0545

    [EDIT] header session exit added

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,8 @@
 <?php
 require "includes/common.php";
 if (!isset($_SESSION['email'])) {
-    header('locationS: index.php');
+    header('Location: index.php');
+    exit();
 }
 ?>
 <header class="header_area header_black">
